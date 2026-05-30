ff34091a34f03c958decc07f1441fc0c92ce60eb
TheDrink-CL <contacto.thedrink@gmail.com>
2026-05-30 00:53:55 -0400

    caja: nueva categoria 'Personal' para pagos a ayudantes

--- src/pages/Caja.js
+++ src/pages/Caja.js
@@ -20,7 +20,7 @@ function ConfirmModal({ mensaje, onConfirm, onCancel }) {
 }
 
 const CATEGORIAS_SALIDA = [
-  'Publicidad', 'Equipamiento', 'Suscripciones', 'Otro gasto'
+  'Publicidad', 'Personal', 'Equipamiento', 'Suscripciones', 'Otro gasto'
 ]
 const CATEGORIAS_ENTRADA = [
   'Venta', 'Delivery', 'Aporte socio', 'Otro ingreso'
